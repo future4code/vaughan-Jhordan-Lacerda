f9ce0362e4258632b9bf18b9f69a84db7841e083
IIVader <47644831+IIVader@users.noreply.github.com>
2021-12-01 15:58:00 -0300

    Exercicio Strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,81 @@
+/*
+1°)
+a. undefined
+b. null
+c. 11
+d. 3
+e. array = [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+f. 9
+
+2°)
+SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+*/
+
+//PRIMEIRA QUESTÃO
+
+// const nomeUsuario = prompt("Digite seu nome:");
+// const emailUsuario = prompt("Digite seu e-mail:");
+
+// console.log(`O e-mail ${emailUsuario} foi cadastrado com sucesso. Seja bem-vindo(a), ${nomeUsuario} `);
+
+
+//SEGUNDA QUESTÃO
+
+//a)
+// const comida =  ["batata-frita", "maçã", "churrasco", "banana", "pizza"];
+
+// console.log(comida);
+
+//b)
+//console.log(`Essas são as minhas comidas preferidas: \n ${comida[0]} \n ${comida[1]} \n ${comida[2]} \n ${comida[3]} \n ${comida[4]}`);
+
+//c)
+// const comidaUsuario = prompt("Digite a sua comida favorita:");
+// comida[1] = comidaUsuario;
+
+// console.log(comida);
+
+
+//TERCEIRA QUESTÃO
+
+//a)
+// let listaDeTarefas = [];
+
+//b)
+// const tarefa = prompt("Digite uma tarefa que precisa realizar:");
+// listaDeTarefas.push(tarefa);
+
+// const tarefa2 = prompt("Digite uma segunda tarefa que precisa realizar:");
+// listaDeTarefas.push(tarefa2);
+
+// const tarefa3 = prompt("Digite uma terceira tarefa que precisa realizar:");
+// listaDeTarefas.push(tarefa3);
+
+//c)
+// console.log(listaDeTarefas);
+
+//d)
+// const tarefaFeita = Number(prompt("Digite o índice de uma tarefa que realizou (de 0 a 2):"));
+
+//e)
+// listaDeTarefas.splice(tarefaFeita, 1);
+
+//f)
+// console.log(listaDeTarefas);
+
+
+//DESAFIO
+
+//PRIMEIRA QUESTÃO
+// const frase = prompt("Digite uma frase:");
+
+// const fraseArray = frase.split([" "]);
+// console.log(fraseArray);
+
+
+//SEGUNDA QUESTÃO
+
+const frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+console.log(`Índice do abacaxi: ${frutas.indexOf("Abacaxi")}, tamanho da array é ${frutas.length}`);
+ 
