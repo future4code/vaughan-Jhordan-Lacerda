2fe09bd72b737ac41992b407023b87acdabf57db
IIVader <47644831+IIVader@users.noreply.github.com>
2021-12-06 16:47:47 -0300

    Subindo respostas do exercício de interpretação e 2 questôes do exercício de escrita de código

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,67 @@
+//Exercícios de interpretação de código
+
+/**
+ * 1°) a)Será impresso: Matheus Nachtergaele, Virginia Cavendish, canal:Globo, horario:14h
+ * 
+ * 2°) a) console.log(cachorro) = nome: Juca, idade: 3, raca: SRD
+ *        console.log(gato) = nome: Juba, idade: 3, raca: SRD
+ *        console.log(tartaruga) = nome: Jubo, idade: 3, raca: SRD
+ *     
+ *     b) ele faz a cópia do objeto cachorro, ou seja, ele copia todas as chaves e valores do objeto antigo para o mais novo.
+ * 
+ * 3°) a) False e undefined
+ *     b) Retornou undefined provavelmente porque não foi definido a propriedade "altura"
+ */
+
+//Exercícios de escrita de código
+
+//Primeira questão
+
+//a)
+// const pessoa = {
+//     nome: "Hiago",
+//     apelidos: ["Presidente", "Hiagão", "Tripé"]
+// }
+
+// const apresentar = (pessoa) => {
+//     console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de ${pessoa.apelidos[0]}, ${pessoa.apelidos[1]} ou ${pessoa.apelidos[2]}`);
+// }
+
+// apresentacao(pessoa);
+
+//b)
+// const novaPessoa = {
+//     ...pessoa,
+//     apelidos: ["Brocador", "Prego", "Musicista"]
+// }
+
+// apresentacao(novaPessoa);
+
+//Segunda questão
+
+//a)
+// const pessoa1 = {
+//     nome: "Jhordan",
+//     idade: 19,
+//     profissao: "estudante"
+// }
+
+// const pessoa2 = {
+//     nome: "Thiago",
+//     idade: 33,
+//     profissao: "Professor"
+// }
+
+//b)
+// const mostrar = (pessoa1, pessoa2) => {
+    
+//     const array1 = [pessoa1.nome, pessoa1.nome.length, pessoa1.idade, pessoa1.profissao, pessoa1.profissao.length];
+//    // console.log(array1);
+//     const array2 = [pessoa2.nome, pessoa2.nome.length, pessoa2.idade, pessoa2.profissao, pessoa2.profissao.length];
+//     //console.log(array2);
+
+//     return array1, array2;
+// }
+
+//mostrar(pessoa1, pessoa2);
+
