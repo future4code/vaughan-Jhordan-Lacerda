ade35a047b1284ef012da5da6e10b68d5b1242dc
IIVader <47644831+IIVader@users.noreply.github.com>
2021-12-11 20:29:13 -0300

    Subindo implementação do blackjack fácil

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,3 +11,26 @@
  * 
  */
 
+let cartaComprada = comprarCarta();
+let cartaComprada2 = comprarCarta();
+
+let cartaCompradaBot = comprarCarta();
+let cartaCompradaBot2 = comprarCarta();
+
+const arrayCartasUser = [cartaComprada, cartaComprada2];
+const arrayCartasBot = [cartaCompradaBot, cartaCompradaBot2];
+
+console.log(cartaComprada.texto, cartaComprada2.texto);
+console.log(cartaCompradaBot.texto, cartaCompradaBot2.texto);
+
+if(cartaComprada.valor === 11 && cartaComprada2.valor === 11){
+   cartaComprada = comprarCarta();
+   cartaComprada2 = comprarCarta();
+   console.log("Entrei no if")
+   console.log(cartaComprada.texto, cartaComprada2.texto);
+} else if(cartaCompradaBot.valor === 11 && cartaCompradaBot2.valor === 11) {
+   cartaCompradaBot = comprarCarta();
+   cartaCompradaBot2 = comprarCarta();
+   console.log("Entrei no if bot")
+   console.log(cartaCompradaBot.texto, cartaCompradaBot2.texto);
+}
--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,3 +10,32 @@
  * 
  * 
  */
+
+console.log("Boas vindas ao jogo de BlackJack!");
+
+if(confirm("Deseja iniciar uma nova rodada?")) {
+   const cartaComprada = comprarCarta();
+   const cartaComprada2 = comprarCarta();
+
+   const valorSomaCartasUser = cartaComprada.valor + cartaComprada2.valor;
+   
+   console.log(`Usuário - cartas: ${cartaComprada.texto} ${cartaComprada2.texto} - pontuação ${valorSomaCartasUser}`);
+
+   const cartaCompradaBot = comprarCarta();
+   const cartaCompradaBot2 = comprarCarta();
+
+   const valorSomaCartasBot = cartaCompradaBot.valor + cartaCompradaBot2.valor;
+
+   console.log(`Computador - cartas: ${cartaCompradaBot.texto} ${cartaCompradaBot2.texto} - pontuação ${valorSomaCartasBot}`)
+
+   if(valorSomaCartasUser > valorSomaCartasBot) {
+      console.log("O usuário ganhou!")
+   } else if(valorSomaCartasBot > valorSomaCartasUser){
+      console.log("O computador ganhou!")
+   }else {
+      console.log("Empate")
+   }
+
+} else {
+   console.log("O jogo acabou!")
+}
