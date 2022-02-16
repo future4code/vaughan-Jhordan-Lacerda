b187c04e92261582e62410ee731f0bbfa3d35e54
IIVader <47644831+IIVader@users.noreply.github.com>
2022-02-16 14:37:32 -0300

    Adicionando lógica de autenticação a página de login

--- modulo3/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
+++ modulo3/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
@@ -1,21 +1,63 @@
-import react from "react";
+import react, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../../constants/urls";
 
 export default function LoginPage() {
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const goBack = () => {
-        navigate("/");
-    }
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <h2>Login</h2>
-            <input placeholder="E-mail"/>
-            <input placeholder="Senha"/>
-            <button onClick={goBack}>Voltar</button>
-            <button>Entrar</button>
-        </div>
-        
-    )
+  const goBack = () => {
+    navigate("/");
+  };
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmitLogin = () => {
+    const url = `${BASE_URL}/login`;
+
+    const body = {
+      email: email,
+      password: password,
+    };
+
+    axios.post(url, body)
+    .then((res) => {
+        alert("Login realizado com sucesso")
+        localStorage.setItem("token", res.data.token)
+        navigate("/admin/trips/list")
+    })
+    .catch((err) => {
+        console.log(err.response)
+        alert(err.response.data.message)
+    });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        placeholder="E-mail"
+        type={"email"}
+        value={email}
+        onChange={onChangeEmail}
+      />
+      <input
+        placeholder="Senha"
+        type={"password"}
+        value={password}
+        onChange={onChangePassword}
+      />
+      <button onClick={goBack}>Voltar</button>
+      <button onClick={onSubmitLogin}>Entrar</button>
+    </div>
+  );
 }
