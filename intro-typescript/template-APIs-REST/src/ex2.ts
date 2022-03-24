8fecadc05489a637b266fea5b6c2b76504600848
IIVader <47644831+IIVader@users.noreply.github.com>
2022-03-24 17:46:05 -0300

    Subindo exercicios completos

--- intro-typescript/template-APIs-REST/src/ex2.ts
+++ intro-typescript/template-APIs-REST/src/ex2.ts
@@ -1,11 +1,7 @@
 function imprimeTresCoresFavoritas():void {
-    const prompt = require('prompt-sync')();
 
-    const cor1: string = prompt("Insira sua primeira cor favorita")
-    const cor2: string = prompt("Insira sua segunda cor favorita")
-    const cor3: string = prompt("Insira sua terceira cor favorita")
+    const cor1: string = process.argv[2]
+    const cor2: string = process.argv[3]
+    const cor3: string = process.argv[4]
     console.log([cor1, cor2, cor3])
 }
-
-
-imprimeTresCoresFavoritas()
