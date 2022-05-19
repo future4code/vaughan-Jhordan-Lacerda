3849f0c36a6baeea7af8669844f5fa2f02561c2e
IIVader <47644831+IIVader@users.noreply.github.com>
2022-05-19 20:15:41 -0300

    Alterando consulta getAllPokemons

--- Redfox/src/data/PokemonDatabase.ts
+++ Redfox/src/data/PokemonDatabase.ts
@@ -5,7 +5,7 @@ export class PokemonDatabase extends BaseDatabase implements IPokeData {
   private TABLE_NAME = "Pokemon_Go";
 
   async getAllPokemons(): Promise<any[]> {
-    const pokemon = await this.getConnection().from(this.TABLE_NAME).limit(5);
+    const pokemon = await this.getConnection().select("Name", "Type_1", "Type_2").from(this.TABLE_NAME).limit(5);
 
     return pokemon;
   }
